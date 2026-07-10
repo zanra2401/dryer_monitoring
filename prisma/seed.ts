4d665895f96624ee7f08a3cee42708e138f35ffa
dhyos <158055171+dhyos@users.noreply.github.com>
2026-07-10 12:49:38 +0700

    pembuatan seed user

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -14,7 +14,32 @@ const adapter = new PrismaMariaDb({
 const prisma = new PrismaClient({ adapter });
 
 async function main() {
-    
+    console.log("[seed] checking if admin user exists...");
+    const existingAdmin = await prisma.user.findUnique({
+        where: {
+            username: "admin",
+        },
+    });
+
+    if (existingAdmin) {
+        console.log("[seed] admin user already exists.");
+        return;
+    }
+
+    console.log("[seed] hashing password...");
+    const hashedPassword = await hashUserPassword("admin123");
+
+    console.log("[seed] creating admin user...");
+    const newAdmin = await prisma.user.create({
+        data: {
+            username: "admin",
+            password: hashedPassword,
+            fullName: "Administrator",
+            role: "ADMIN",
+        },
+    });
+
+    console.log(`[seed] successfully created initial admin user: ${newAdmin.username}`);
 }
 
 main()
